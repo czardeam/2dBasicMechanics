880d5c7e04cbbb01e6f25396594f7bbde462efe6
Mustafa Balcı <czardeam@gmail.com>
2025-08-06 23:33:06 +0300

    Enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,8 +2,25 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    private SpriteRenderer sr;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+
     public void TakeDamage()
     {
-    
+        sr.color = Color.red;
+
+        Invoke("TurnWhite", 3);
+    }
+
+
+    private void TurnWhite()
+    {
+        sr.color = Color.white;
     }
 }
